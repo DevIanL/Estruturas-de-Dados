1574b8a6628755ae65a9c25b828c7ce252eda50a
DevIanL <160551044+DevIanL@users.noreply.github.com>
2025-09-01 18:02:51 -0300

    commit16

--- Pilhas/PilhaDupla/src/Main.java
+++ Pilhas/PilhaDupla/src/Main.java
@@ -4,30 +4,30 @@ public class Main {
     public static void main(String[] arg){
         PilhaDupla a = new PilhaDupla();
 
+        a.empilhar(1, "Pablo");
         System.out.println(a.imprimir());
 
-        a.empilhar(1, "p1=1");
-        a.empilhar(2, "p2=1");
-        System.out.println(a.imprimir());
-
-        a.empilhar(1, "p1=2");
-        a.empilhar(1, "p1=3");
-        a.empilhar(1, "p1=4");
+        a.empilhar(2, "Pablo");
         System.out.println(a.imprimir());
 
         a.desempilhar(1);
         System.out.println(a.imprimir());
-
-        a.empilhar(1, "p1=4.1");
+        
+        a.empilhar(1, "Ian");
+        a.empilhar(1, "Ian");
+        a.empilhar(1, "Ian");
+        a.empilhar(1, "Ian");
+        a.empilhar(1, "Ian");
+        a.empilhar(1, "Ian");
+        a.empilhar(1, "Ian");
+        a.empilhar(1, "Ian");
+        a.empilhar(1, "Ian");
         System.out.println(a.imprimir());
 
-        a.empilhar(2, "p2=2");
-        a.empilhar(2, "p2=3");
-        a.empilhar(2, "p2=4");
-        a.empilhar(2, "p2=5");
-        a.empilhar(2, "p2=6");
+        a.empilhar(2, "Pablo");
+        a.desempilhar(1);
+        a.empilhar(2, "Pablo");
         System.out.println(a.imprimir());
 
-        a.empilhar(2, "p2=6");
     }
 }
--- Pilhas/PilhaEstatica/src/PilhaEstaticaPrincipal.java
+++ Pilhas/PilhaEstatica/src/PilhaEstaticaPrincipal.java
@@ -2,11 +2,25 @@ package src;
 
 public class PilhaEstaticaPrincipal {
     public static void main(String[] args) {
-        PilhaEstatica a = new PilhaEstatica();
-        a.empilhar("oi");
-        a.empilhar("tim");
+        PilhaEstatica a = new PilhaEstatica(9);
+
         System.out.println(a.imprimir());
-        a.desempilhar();
+        a.empilhar("Pablo");
         System.out.println(a.imprimir());
+        a.empilhar("Penga");
+        System.out.println(a.imprimir());
+        System.out.println(a.desempilhar());
+        System.out.println(a.imprimir());
+        a.empilhar("Pablo");
+        a.empilhar("Pablo");
+        a.empilhar("Pablo");
+        a.empilhar("Pablo");
+        a.empilhar("Pablo");
+        a.empilhar("Pablo");
+        a.empilhar("Pablo");
+        a.empilhar("Pablo");
+        System.out.println(a.imprimir());
+
+        a.empilhar("Pablo");
     }
 } 
